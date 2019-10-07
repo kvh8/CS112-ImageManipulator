349803e1537aa5c97f79cd50e73d9ce414b99c6d
kvh8 <54556865+kvh8@users.noreply.github.com>
2019-10-07 14:12:47 -0700

    image manipulator 10/07/2019

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 
 	ifstream infile;
 	ofstream outfile;
-	string choice;
+	int choice;
 	string line1;
 	string line2;
 	string line3;
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 	cout << "Provide the name of the file you would like to write to" << endl;
 	cin >> out_file_name;
 
-	/*cout << "Select the image effect you want:" << endl
+	cout << "Select the image effect you want:" << endl
 		<< "***Image Effects * **" << endl
 		<< "1. Remove Red" << endl
 		<< "2. Remove Blue" << endl
@@ -41,7 +41,7 @@ int main(int argc, char* argv[])
 		<< "7. Grayscale" << endl
 		<< "*** Choose the number of the effect you want (1 = remove red)" << endl;
 	cin >> choice;
-	*/
+	
 
 
 	// Reading the file
@@ -92,7 +92,8 @@ int main(int argc, char* argv[])
 
 	//Remove Red
 
-	/*for (int i = 0; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 0; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 0;
 	}
@@ -100,7 +101,8 @@ int main(int argc, char* argv[])
 
 	//Remove Green
 
-	/*for (int i = 1; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 1; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 0;
 	}
@@ -108,7 +110,8 @@ int main(int argc, char* argv[])
 
 	//Remove Blue
 
-	/*for (int i = 2; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 2; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 0;
 	}
@@ -116,7 +119,8 @@ int main(int argc, char* argv[])
 
 	//Negate Red
 
-	/*for (int i = 0; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 0; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 255 - data_vector[i];
 	}
@@ -124,7 +128,8 @@ int main(int argc, char* argv[])
 
 	//Negate Green
 
-	/*for (int i = 1; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 1; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 255 - data_vector[i];
 	}
@@ -132,7 +137,8 @@ int main(int argc, char* argv[])
 
 	//Negate Blue
 
-	/*for (int i = 2; i < data_vector.size(); i += 3)
+	/*
+	for (int i = 2; i < data_vector.size(); i += 3)
 	{
 		data_vector[i] = 255 - data_vector[i];
 	}
@@ -140,19 +146,100 @@ int main(int argc, char* argv[])
 
 	//Grayscale
 
-	for (int i = 0; i < data_vector.size(); i += 3)
+	/*for (int i = 0; i < data_vector.size(); i += 3)
 	{
 		int red = i;
 		int green = i + 1;
 		int blue = i + 2;
-		int average = (red + green + blue) / 3;
+		int average = (data_vector[red] + data_vector[green] + data_vector[blue]) / 3;
 
-		for (int j = 0; j < 2; j++)
+		//Maybe if statement instead
+
+		if (data_vector[red] != average || data_vector[green] != average || data_vector[blue] != average)
+		{
+			data_vector[red] = average;
+			data_vector[green] = average;
+			data_vector[blue] = average;
+		}
+	
+		/*
+		**Problem with this was it only changed the first three values and never went to next three
+		for (int j = 0; j <= 2; j++)
 		{
 			data_vector[j] = average;
 		}
+		
 	}
+	*/
+
+	// My switch case to change their choice to the effect they want
+
+	switch (choice)
+	{
+	case 1:			//Remove Red
+		 for (int i = 0; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 0;
+		}
+	break;
+
+	case 2:			//Remove Green
+		for (int i = 1; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 0;
+		}
+	break;
+
+	case 3:			//Remove Blue
+		for (int i = 2; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 0;
+		}
+	break;
+
+	case 4:			//Negate Red
+		for (int i = 0; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 255 - data_vector[i];
+		}
+	break;
+
+	case 5:			//Negate Green
+		for (int i = 1; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 255 - data_vector[i];
+		}
+	break;
 	
+	case 6:			//Negate Blue
+		for (int i = 2; i < data_vector.size(); i += 3)
+		{
+			data_vector[i] = 255 - data_vector[i];
+		}
+	break;
+
+	case 7:			//Grayscale
+		for (int i = 0; i < data_vector.size(); i += 3)
+		{
+			int red = i;
+			int green = i + 1;
+			int blue = i + 2;
+			int average = (data_vector[red] + data_vector[green] + data_vector[blue]) / 3;
+
+			if (data_vector[red] != average || data_vector[green] != average || data_vector[blue] != average)
+			{
+				data_vector[red] = average;
+				data_vector[green] = average;
+				data_vector[blue] = average;
+			}
+		}
+	break;
+
+	default:
+		cout << "Error in choice, Please choose a number between 1 and 7" << endl;
+
+	}
+
 
 	infile.close();
 	
